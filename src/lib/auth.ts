d456d7a6bde300070835138b64ca35ce841b600b
habiburRhaman05 <devhabib2005@gmail.com>
2026-01-02 20:24:05 +0600

    added google login, and added google oAuth client information

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -91,10 +91,11 @@ export const auth = betterAuth({
         },
 
     socialProviders: {
-        google: {
-            clientId: "",
-            clientKey: "",
-            clientSecret: ""
-        }
+    google: {
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        accessType: "offline", 
+        prompt: "select_account consent", 
+    },
     }
 });
