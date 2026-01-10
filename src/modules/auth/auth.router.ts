9e182c2b45bca79ec79cf3b179b435e240782916
habiburRhaman05 <devhabib2005@gmail.com>
2026-01-10 18:50:04 +0600

    delete auth module

--- src/modules/auth/auth.router.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { authControllers } from "./auth.controller";
-
-const router = express.Router();
-router.post("/forgot-password", authControllers.forgotPassword);
-router.post("/reset-password", authControllers.resetPassword);
-
-export default router;
